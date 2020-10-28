ce3525c789650290169d8ea67c826bcc04318b7b
ivanmijacika <imijacika20@stuy.edu>
2020-10-27 21:52:04 -0400

    final changes made

--- BankTest.java
+++ BankTest.java
@@ -2,8 +2,9 @@ public class BankTest{
 
     public static void main(String[] args){
 	BankAccount account1 = new BankAccount(12345, "blackclover");
-	System.out.println(account1.getBalance());
+	System.out.println(account1.toString());
 
+	System.out.println(account1.getBalance());
 	System.out.println(account1.deposit(-100));
         System.out.println(account1.deposit(100));
 	System.out.println(account1.getBalance());
@@ -14,6 +15,13 @@ public class BankTest{
 	System.out.println(account1.withdraw(90));
 	System.out.println(account1.getBalance());
 	System.out.println(account1.withdraw(10));
+
+	System.out.println(account1.getAccountID());
+
+	account1.setPassword("HxH"); //Doesn't do anything visible?
+
+	System.out.println(account1.deposit(69420));
+	System.out.println(account1.toString());
 
     }
 
