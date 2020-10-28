104f37b7a9b5e49fe33b123a26ae9075638a2357
ivanmijacika <imijacika20@stuy.edu>
2020-10-27 20:58:32 -0400

    deposit and withdraw added and Test class created

--- BankAccount.java
+++ BankAccount.java
@@ -21,4 +21,21 @@ public class BankAccount{
     public void setPassword(String pd){
         password = pd;
     }
+
+    public boolean deposit(double amount){
+        if (amount>=0){
+            balance += amount;
+            return true;
+        }
+        else return false;
+    }
+
+    public boolean withdraw(double amount){
+        if (balance>=amount && amount>0){
+            balance -= amount;
+            return true;
+        }
+        else return false;
+    }
+
 }
