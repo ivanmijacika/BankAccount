2cafc4aec0a17d44fcce3f59137041e57b22e72f
ivanmijacika <imijacika20@stuy.edu>
2020-10-27 21:32:39 -0400

    2nd test update

--- BankTest.java
+++ BankTest.java
@@ -3,11 +3,18 @@ public class BankTest{
     public static void main(String[] args){
 	BankAccount account1 = new BankAccount(12345, "blackclover");
 	System.out.println(account1.getBalance());
+
+	System.out.println(account1.deposit(-100));
         System.out.println(account1.deposit(100));
 	System.out.println(account1.getBalance());
+
         System.out.println(account1.withdraw(-10));
         System.out.println(account1.withdraw(10));
 	System.out.println(account1.getBalance());
+	System.out.println(account1.withdraw(90));
+	System.out.println(account1.getBalance());
+	System.out.println(account1.withdraw(10));
+
     }
 
 }
