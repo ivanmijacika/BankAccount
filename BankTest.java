41c8e317db1f25058705b99c52b095851711598d
ivanmijacika <imijacika20@stuy.edu>
2020-10-27 21:18:10 -0400

    Created an account to test

--- BankTest.java
+++ BankTest.java
@@ -1,2 +1,12 @@
 public class BankTest{
+
+    public static void main(String[] args){
+	BankAccount account1 = new BankAccount(100.00, 12345, "blackclover");
+	//System.out.println();
+        //System.out.println();
+        //System.out.println();
+        //System.out.println();
+
+    }
+
 }
