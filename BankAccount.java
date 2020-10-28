eac4db348c909ee566b65a2f63a16fc3c145d754
ivanmijacika <imijacika20@stuy.edu>
2020-10-27 20:44:45 -0400

    first real commit

---
+++ BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount{
+
+    private double balance;
+    private int accountID;
+    private String password;
+
+    public BankAccount(double b, int a, String p){
+	balance = b;
+	accountID = a;
+	password = p;
+    }
+
+}
